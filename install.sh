634461b1a56124480ca0e89f5fb9453610a3206a
Patrick Wang <pwang@mozilla.com>
2012-10-19 18:26:10 +0800

    Add basic bash

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function install_bash {
+    echo "Installing bash config..."
+
+    echo "### Added by Patrick's config ###" >> $HOME/.bashrc
+    cat $SCRIPT_DIR/bashrc >> $HOME/.bashrc
+    echo "### End Patrick's config ###" >> $HOME/.bashrc
+    echo "### Added by Patrick's config ###" >> $HOME/.profile
+    cat $SCRIPT_DIR/profile >> $HOME/.profile
+    echo "### End Patrick's config ###" >> $HOME/.profile
+
+    echo "    Done!"
+}
+
+## Main
+
+install_bash
