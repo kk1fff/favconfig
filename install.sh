d2c995c443c37e3e4867a2877e4c093dc7ac1317
kk1fff <kk1fff@patrickz.net>
2012-11-30 01:03:01 +0800

    Update to fit apple

--- install.sh
+++ install.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASHRC=
+PROFILE=
+
+function decide_filenames {
+    BASHRC=$HOME/.bashrc
+    if [ -r $HOME/.bash_profile ]; then
+        PROFILE=$HOME/.bash_profile
+    else
+        PROFILE=$HOME/.profile
+    fi
+}
 
 function install_bash {
     echo "Installing bash config..."
 
-    echo "### Added by Patrick's config ###" >> $HOME/.bashrc
-    cat $SCRIPT_DIR/bashrc >> $HOME/.bashrc
-    echo "" >> $HOME/.bashrc
-    echo "### End Patrick's config ###" >> $HOME/.bashrc
+    echo "Writing to rc file: $BASHRC"
+    echo "### Added by Patrick's config ###" >> $BASHRC
+    cat $SCRIPT_DIR/bashrc >> $BASHRC
+    echo "" >> $BASHRC
+    echo "### End Patrick's config ###" >> $BASHRC
 
 
-    echo "### Added by Patrick's config ###" >> $HOME/.profile
-    cat $SCRIPT_DIR/profile >> $HOME/.profile
-    echo "" >> $HOME/.profile
-    echo "### End Patrick's config ###" >> $HOME/.profile
+    echo "Writing to profile file: $PROFILE"
+    echo "### Added by Patrick's config ###" >> $PROFILE
+    cat $SCRIPT_DIR/profile >> $PROFILE
+    ## Also, write bashrc.
+    cat $SCRIPT_DIR/bashrc >> $PROFILE
+    echo "" >> $PROFILE
+    echo "### End Patrick's config ###" >> $PROFILE
 
     echo "    Done!"
 }
 
 ## Main
 
+decide_filenames
 install_bash
