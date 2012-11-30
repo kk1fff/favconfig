457b35b829da5fc3cb84f79d3f13fe1e318586d5
kk1fff <kk1fff@patrickz.net>
2012-11-30 11:25:18 +0800

    Add per-system configure

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASHRC=
 PROFILE=
+SYSTEM=`uname -s`
 
 function decide_filenames {
     BASHRC=$HOME/.bashrc
@@ -13,12 +14,36 @@ function decide_filenames {
     fi
 }
 
+function write_bashrc_by_os {
+    local file=$SCRIPT_DIR/bashrc_$SYSTEM
+    echo "- OS Specified file is: $file"
+    if [ -r $file ]; then
+        echo "   Writing"
+        cat $file >> $BASHRC
+    else
+        echo "   Not found"
+    fi
+}
+
+function write_profile_by_os {
+    local file=$SCRIPT_DIR/profile_$SYSTEM
+    echo "- OS Specified file is: $file"
+    if [ -r $file ]; then
+        echo "    Writing"
+        cat $file >> $PROFILE
+    else
+        echo "    Not found"
+    fi
+}
+
 function install_bash {
     echo "Installing bash config..."
+    echo "  Current system is: $SYSTEM"
 
     echo "Writing to rc file: $BASHRC"
     echo "### Added by Patrick's config ###" >> $BASHRC
     cat $SCRIPT_DIR/bashrc >> $BASHRC
+    write_bashrc_by_os
     echo "" >> $BASHRC
     echo "### End Patrick's config ###" >> $BASHRC
 
@@ -26,12 +51,11 @@ function install_bash {
     echo "Writing to profile file: $PROFILE"
     echo "### Added by Patrick's config ###" >> $PROFILE
     cat $SCRIPT_DIR/profile >> $PROFILE
-    ## Also, write bashrc.
-    cat $SCRIPT_DIR/bashrc >> $PROFILE
+    write_profile_by_os
     echo "" >> $PROFILE
     echo "### End Patrick's config ###" >> $PROFILE
 
-    echo "    Done!"
+    echo "- Done!"
 }
 
 ## Main
